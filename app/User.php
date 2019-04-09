aab08e3eccac27175be766b3b03bbf53686804d9
hoanghongchuong <chuonghoanhong@gmail.com>
2019-04-09 11:02:58 +0700

    generate question model via artisan model generator

--- app/User.php
+++ app/User.php
@@ -26,4 +26,19 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * relationship question
+     */
+    public function questions()
+    {
+        
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
